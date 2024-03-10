c85c7d6e54196c3b59790d5cb5a04aab68eb5c11
Peter Eijgermans <p.eijgermans11@gmail.com>
2024-03-10 22:39:32 +0100

    define declarations for communication and Dynamic loading component

--- module-federation-plugin-example/projects/mfe1/src/app/app.component.ts
+++ module-federation-plugin-example/projects/mfe1/src/app/app.component.ts
@@ -1,14 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AuthService } from '@demo/auth';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
 })
 export class AppComponent implements OnInit {
   @Input() expectedAppState: string;
   @Output() onAppStateChanged = new EventEmitter<string>();
+
+  // declarations IS USED FOR COMMUNICATION WITH MODULE FEDERATION 
+  static declarations = AppComponent;
 
   private previousExpectedAppState: string = '';
   public auth = inject(AuthService);
