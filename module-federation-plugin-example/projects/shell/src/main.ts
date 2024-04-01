46f3a399dcbc929cf4ee417d239bb313aa187e7c
Peter Eijgermans <p.eijgermans11@gmail.com>
2024-04-01 18:21:13 +0200

    multiple manifest files

--- module-federation-plugin-example/projects/shell/src/main.ts
+++ module-federation-plugin-example/projects/shell/src/main.ts
@@ -1,6 +1,30 @@
+
 import { initFederation } from '@angular-architects/native-federation';
 
-initFederation('/assets/manifests/federation.manifest.json')
-  .catch(err => console.error(err))
-  .then(_ => import('./bootstrap'))
-  .catch(err => console.error(err));
+let manifestEnv = 'dev';
+
+switch (window.location.hostname) {
+   case 'http://prod.nl':
+       manifestEnv = 'prod';
+       break;
+    case 'http://acc.nl':
+        manifestEnv = 'acc';
+        break;
+    case 'http://test.nl':
+        manifestEnv = 'test';
+        break;
+   default:
+       manifestEnv = 'local';
+       break;
+}
+
+let location = `/assets/manifests/manifest.${manifestEnv}.json`;
+
+if (manifestEnv === 'prod' || manifestEnv === 'bpr' || manifestEnv === 'ont') {
+    location = window.location.pathname + location;
+}
+
+initFederation(location)
+    .catch(err => console.error('failed', err))
+    .then(_ => import('./bootstrap'))
+    .catch(err => console.error(err));
